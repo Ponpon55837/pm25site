72661f1194a892659010d5d06202f8e98a9708fe
Upfloor5 <32410486+Ponpon55837@users.noreply.github.com>
2020-12-30 16:15:55 +0800

    add some resource

--- src/composables/getData.js
+++ src/composables/getData.js
@@ -16,7 +16,7 @@ const getData = () => {
     // }
     await fetch(`https://data.epa.gov.tw/api/v1/aqx_p_02?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&format=json`)
       .then(res => res.json())
-      .then(data => pmData.value = data )
+      .then(data => pmData.value = data)
       .catch(err => error.value = err.message)
   }
   // 這裡return的原因是因為為了在被使用時，可以用解構的方式呼叫
