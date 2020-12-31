ee215b270e7a1878e74f68470f471bf11c452a51
Upfloor5 <32410486+Ponpon55837@users.noreply.github.com>
2020-12-31 08:53:06 +0800

    add some files update

---
+++ src/views/home/SingleHome.vue
@@ -0,0 +1,16 @@
+<template>
+  <div @click="showHome = false" class="backdrop">
+    <div class="dropcontent">
+      <h2>縣市名稱：{{ site.county }}</h2>
+      <label>測站名稱：{{ site.Site }}</label>
+      <p>PM 2.5 濃度：{{ site.PM25 }} {{ site.ItemUnit }}</p>
+      <p>記錄時間： {{ site.DataCreationDate }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['site', 'showHome']
+}
+</script>
