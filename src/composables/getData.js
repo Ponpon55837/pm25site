48e8b7a568ad3f28d663bcb6af5fb06ae0888528
Upfloor5 <32410486+Ponpon55837@users.noreply.github.com>
2021-01-15 09:09:42 +0800

    add axios fetch

--- src/composables/getData.js
+++ src/composables/getData.js
@@ -1,16 +1,24 @@
 import { ref } from 'vue'
+import axios from 'axios'
 
 const getData = () => {
   const originData = ref([])
   const error = ref(null)
   const jsonHandler = async (url) => {
-    try {
-      const response = await fetch(url);
-      const result = await response.json()
-      return originData.value = result.records
-    } catch (err) {
+    axios.get(url)
+    .then(res => {
+      return originData.value = res.data.records
+    })
+    .catch(err => {
       return error.value = err.message
-    }
+    })
+    // try {
+    //   const response = await fetch(url);
+    //   const result = await response.json()
+    //   return originData.value = result.records
+    // } catch (err) {
+    //   return error.value = err.message
+    // }
   }
   // 這裡return的原因是因為為了在被使用時，可以用解構的方式呼叫
   return { originData, error, jsonHandler }
