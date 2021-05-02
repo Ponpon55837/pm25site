bb2a70f4619519369a58794ca6a7b725b49eb493
upfloor5 <32410486+Ponpon55837@users.noreply.github.com>
2021-05-02 16:44:29 +0800

    add flex to loading

--- src/outerComponents/Loading.vue
+++ src/outerComponents/Loading.vue
@@ -1,7 +1,7 @@
 <template>
   <div 
     v-if="!matchContent.length && searchContent === '' && loadState" 
-    class="flex">
+    class="flex loading">
     <svg 
         xmlns="http://www.w3.org/2000/svg" 
         xmlns:xlink="http://www.w3.org/1999/xlink" 
